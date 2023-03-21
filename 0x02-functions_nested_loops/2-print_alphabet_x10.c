7f670e228b7949e10e23110063b32f30116b16c9
davoyesh <oyindola.esho@gmail.com>
2023-03-21 11:50:16 -0700

    Prints 10x alphabets in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,10 @@
 
 void print_alphabet_x10(void)
 {
-	int ten;
-	char la;
+	char i;
 
-	for (ten = 0; ten <= 9; ten++)
-	{
-		for (la = 'a'; la <= 'z' la++)
-			_putchar(la);
-		_putchar("\n");
-	}
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar("\n");
 }
