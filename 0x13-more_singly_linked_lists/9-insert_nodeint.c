c56388078e6793dd5b34c478da0ecf095610c308
Davoyesh <oyindola.esho@gmail.com>
2023-05-01 01:10:53 -0700

    function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,47 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a
- * given position in a listint_t list.
- * @head: A pointer to a pointer to the
- * head node of the linked list.
- * @idx: The index of the list where the new node should be added.
+ * insert_nodeint_at_index - Inserts a new node at a given position.
+ * @head: Pointer to a pointer to the beginning of the listint_t list.
+ * @idx: Index of the list where the new node should be added.
  * Index starts at 0.
- * @n: The data to be stored in the new node.
+ * @n: Value to store in the new node.
  *
- * Return: The address of the new node,
- * or NULL if it failed to add the new node.
+ * Return: Address of the new node, or NULL if it failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new_node, *temp;
 	unsigned int i = 0;
-	listint_t *new_node, *current;
-
-	if (head == NULL)
-		return (NULL);
-
-	if (idx == 0)
-		return (add_nodeint(head, n));
-
-	current = *head;
-
-	while (current != NULL && i < idx - 1)
-	{
-		current = current->next;
-		i++;
-	}
-
-	if (i != idx - 1)
-		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = current->next;
-	current->next = new_node;
 
-	return (new_node);
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		if (i == idx - 1)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+
+		i++;
+		temp = temp->next;
+	}
+
+	free(new_node);
+	return (NULL);
 }
